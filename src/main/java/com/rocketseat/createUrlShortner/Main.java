7aefa1c3ca4b1c946121ee8626028a8c04a6992f
zlucasftw <ilucasmcpvp@gmail.com>
2024-11-18 12:50:49 -0300

    wip: Aula 1 - Criando Funcao Serveless e Configurando URL Encurtada

---
+++ src/main/java/com/rocketseat/createUrlShortner/Main.java
@@ -0,0 +1,38 @@
+package com.rocketseat.createUrlShortner;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        String body = input.get("body").toString();
+
+        Map<String, String> bodyMap;
+        try {
+            bodyMap = objectMapper.readValue(body, Map.class);
+
+        } catch (JsonProcessingException exception) {
+            throw new RuntimeException("Error parsing JSON body: " + exception.getMessage(), exception);
+        }
+
+        String originalUrl = bodyMap.get("originalUrl");
+        String expirationTime = bodyMap.get("expirationTime");
+
+        String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("code", shortUrlCode);
+
+        return response;
+    }
+}
